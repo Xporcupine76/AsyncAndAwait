68444e9cac99453569e3ea7aa3119f8fa34ce8ba
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-10 11:54:51 +0200

    Add project files.

---
+++ AsyncAndAwait/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml.Xsl;
+
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            AsynceAwaitDemo demo = new AsynceAwaitDemo();
+            demo.DoStuff();
+
+            for(int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Working on the main Thread...");
+
+            }
+           
+        }
+
+
+    }
+    public class AsynceAwaitDemo
+    {
+
+        public async Task DoStuff()
+        {
+           /* await Task.Run(() =>
+            {
+                CountToFifty();
+
+            });
+           */ 
+            Console.WriteLine("Counting to 50 is Compiled");
+
+
+        }
+
+
+        private static async Task<string> CountToFifty()
+        {
+
+            int counter;
+            for (counter = 0; counter < 51; counter++) ;
+            {
+                Console.WriteLine("BG thread: " + counter);
+            }
+        }
+    }  
+
+}
